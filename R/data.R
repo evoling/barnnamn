61739b9e0a23fee22552601b62843d45e487c17b
Michael Dunn <michael.dunn@lingfil.uu.se>
2019-04-06 10:50:51 +0200

    Fixed documentation

--- R/data.R
+++ R/data.R
@@ -9,6 +9,7 @@
 #'   \item{\code{sex}}{Names are listed separately by sex}
 #'   \item{\code{name}}{Full given names according to the Swedish national register}
 #'   \item{\code{n}}{Number of times name was used (by sex and year); names used less than 10 times have been excluded for privacy reasons}
+#'   \item{\code{prop}}{Name as proportion of all names (by sex and year); includes names used than 10 times, so sum of proportions is < 1.0}
 #' }
 #' @source \url{https://www.scb.se/hitta-statistik/statistik-efter-amne/befolkning/amnesovergripande-statistik/namnstatistik/}
 "barnnamn"
